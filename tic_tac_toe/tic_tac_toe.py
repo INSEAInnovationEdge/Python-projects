97279afa14f59e44af190575179c1feb02d8794a
MahmoudFettal <MahmoudFettal@users.noreply.github.com>
2021-02-19 22:22:58 +0100

    tic tac toe

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,119 +1,125 @@
 '''
-this file contains all the functions that contribute in the making of tic tac toe 
-
-__________ about the game __________
-         1-the grid shape :
-
-              1 | 2 | 3
-             -----------
-              4 | 5 | 6
-             -----------
-              7 | 8 | 9
-
-       2-how to play the game :
-
-    the player with x starts first and 
-must choose a case number.
-____________________________________
-
-autour: Samuj
+by mahmoud fettal in the 12/25/2020
 '''
 
-def win_check(x_cells, o_cells, the_round):
-    ''' this function checks if the player that played this round won or not
-    -input:
-    x_cells : the cells taken by 'x' player
-    o_cells : the cells taken by 'o' player
-    the_round : the round we are in 
+import pygame,sys,os
 
-    -ouput:
-    returns True if the player won or not
-    '''
-    
-    player_cells = x_cells if the_round%2 == 1 else o_cells
+class Settings():
+    screen_size = (400,500)
+    width       = 100
+    frames      = {i: pygame.image.load(os.path.join('frames', f'{i}.png')) for i in 'X,O,E,grid,Draw,O to play,X to play,reset,title,X wins,O wins'.split(',')}
+    bg_color    = pygame.Color('#FFFFFF')
 
-    lines   = [(1+(i*3), 2+(i*3), 3+(i*3)) for i in range(3)]
-    columns = [(i, i+3 , i+6) for i in range(1,4)]
-    diags   = [(1,5,9),(7,5,3)]
+class Grid():
+    def __init__(self):
+        self.x_cells   = set()
+        self.o_cells   = set()
+        self.the_round = 1
+        self.ended     = False
 
-    win_postions = [set(position) for layout in [lines, columns, diags] for position in layout]
+    def win_check(self):
+        ''' this function checks if the player that played this round won or not
+        -ouput:
+        returns True if the player won or not
+        '''
+        
+        player_cells = self.x_cells if self.the_round%2 == 1 else self.o_cells
 
-    for position in win_postions:
-        if position.issubset(player_cells):
-            player = 'x' if the_round%2 == 1 else 'o'
-            show(x_cells, o_cells) 
-            print(f'\n{player} wins !')
-            return True 
+        lines   = [(1+(i*3), 2+(i*3), 3+(i*3)) for i in range(3)]
+        columns = [(i, i+3 , i+6) for i in range(1,4)]
+        diags   = [(1,5,9),(7,5,3)]
 
-def show(x_cells, o_cells):
-    ''' 
-    -input:
-    x_cells : the cells taken by 'x' player
-    o_cells : the cells taken by 'o' player
+        win_postions = [set(position) for layout in [lines, columns, diags] for position in layout]
 
-    -ouput:
-    prints the layout of the board shape above (doesn't return anything)
-    '''
-     
-    def charcter(x):
-        if x in x_cells:
-            return ' x '
-        elif x in o_cells:
-            return ' o '
-        return '   '
-    print('\n-----------\n'.join('|'.join(list(map(charcter,(1+(i*3), 2+(i*3), 3+(i*3))))) for i in range(3)))
+        for position in win_postions:
+            if position.issubset(player_cells):
+                player = 'X' if self.the_round%2 == 1 else 'O'
+                self.ended = True 
+                return f'{player} wins'
 
 
-def valid_cell(x_cells, o_cells):
-    ''' this function checks if an input is valid or not, for that to be 
-    true it must be a digit in range(1,10) and not included in x_cells or 
-    o_cells. 
-    -input:
-    x_cells : the cells taken by 'x' player
-    o_cells : the cells taken by 'o' player
+        if self.the_round == 9:
+            self.ended = True 
+            return 'Draw'
 
-    -ouput:
-    return the valid cell entered by the user 
-    '''
+class Tic_tac_toe():
+    def __init__(self):
+        self.game     = Grid()
+        self.settings = Settings()
+        self.winner   = 'Draw'
+        
 
-    while True:
-        cell = input('what cell do you choose: ')
-    if cell.isdigit():
-        cell = int(cell)
-        if not cell in x_cells and not cell in o_cells and 1 <= cell <= 9:
-            print()
-            return cell
-        else:
-            print('not available, try again !')
-    else:
-        print('give a valid number, try again !')
-    
+    def turn(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                n,m = pygame.mouse.get_pos()
+                n = (n-50)//100
+                m = (m-125)//100
+                x = m*3 + n + 1
+                if 0 <= n < 3 and 0 <= m < 3 and (not x in self.game.x_cells and not x in self.game.o_cells) :
+                    if self.game.the_round % 2 == 1:
+                        self.game.x_cells.add(x)
+                    else:
+                        self.game.o_cells.add(x)
 
-def tic_tac_toe():
-    ''' this is the body of the game use the function above to make it work'''
+                    self.winner = self.game.win_check()
+                    self.game.the_round += 1
 
-    x_cells = set()
-    o_cells = set()
-    
-    the_round = 1
+    def update(self,screen):
+        def frame(x):
+            if x in self.game.x_cells:
+                return 'X'
+            elif x in self.game.o_cells:
+                return 'O'
+            else:
+                return 'E'
 
-    while the_round < 10:
-        player = 'x' if the_round%2 == 1 else 'o'
-        print(f'\nround {the_round} it is {player} to play \n')
+        screen.fill(self.settings.bg_color)
+        
+        screen.blit(self.settings.frames['title'], (106, 24))       
 
-        cell = valid_cell(x_cells, o_cells)
-        if the_round % 2 == 1:
-            x_cells.add(cell)
-        else:
-            o_cells.add(cell)
-            
-        if win_check(x_cells, o_cells, the_round):
-            return
+        for i in range(3):
+            for j in range(3):
+                x = i*3 + j + 1
+                screen.blit(self.settings.frames[frame(x)], (50 + j*self.settings.width, 125 + i*self.settings.width))    
 
-        show(x_cells, o_cells)        
-        the_round += 1
+        screen.blit(self.settings.frames['grid'], (50,125))
+        if self.game.ended:
+            screen.blit(self.settings.frames[self.winner], (50,455))
+            screen.blit(self.settings.frames['reset'], (301,439))
+            self.game_over()
 
-    print('\ndraw')
+        turn = 'X to play' if self.game.the_round%2 == 1 else 'O to play'
+        screen.blit(self.settings.frames[turn], (150, 75))   
+
+        clock = pygame.time.Clock()
+        clock.tick(60)
+        pygame.display.flip()
+
+    def game_over(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                n,m = pygame.mouse.get_pos()
+                if 300 <= n <= 430 and 439 <= m <= 489 :
+                    self.play()
+
+    def play(self):
+        self.game = Grid()
+        pygame.init()
+        pygame.display.set_caption('tic tac toe')
+        screen = pygame.display.set_mode(self.settings.screen_size)
+        screen.fill(self.settings.bg_color)
+        self.update(screen)
+        while self.game.the_round < 11:
+            self.turn()
+            self.update(screen)
+
+
 
 if __name__ == '__main__':
-    tic_tac_toe()
+    tic_tac_toe = Tic_tac_toe()
+    tic_tac_toe.play()
